3365b724817e62ab5ecf42840e23dace33fdc024
jhilesyoung <jhilesyoung@gmail.com>
2021-01-27 19:26:24 -0500

    finish difficulty buttons and sprites

--- src/index.js
+++ src/index.js
@@ -72,3 +72,131 @@ const background = new Image();
 //     left: 138px;
 //     top: 107px;
 //   }
+
+//3RD ATTEMPT
+
+
+// .game-board {
+//     // display: flex;
+//     // position: relative;
+//     // flex-direction: row;
+//   }
+
+//   .puzzle-and-picture {
+//     // display: flex;
+//     // position: absolute;
+//   }
+
+//   .puzzle-frame {
+//     // display: flex;
+//     // position: absolute;
+//     // height: 94vh;
+//     // width: 70vw;
+//   }
+ 
+//   .frame-and-canvas {
+//     // display: flex;
+//     // position: absolute;
+//     // flex-direction: row;
+//   }
+
+//   #myCanvas {
+//      background-color: aliceblue;
+//      top: 103px;
+//      display: flex;
+//      position: absolute;
+//      left: 141px;
+//      height: 62vh;
+//      width: 49vw;
+//
+// background-image: url("../images/puzzle_frame_transparent.png");
+//    
+//     background-repeat: no-repeat;
+//     background-position: center center;
+//     background-size: contain; 
+//   }
+
+//   body > div > div.frame-and-canvas > div {
+//     //   display: flex;
+//     //   position: absolute;
+//     //   flex-direction: column;
+//   }
+
+//FIGURE OUT THE BACKGROUND LATER 
+// background-image: url("../images/puzzle_frame_transparent.png");
+//    
+//     background-repeat: no-repeat;
+//     background-position: center center;
+//     background-size: contain; 
+
+
+
+// 4TH WITH DIFFICULTY 
+
+// button:hover {
+//     cursor: pointer;
+// }
+
+// html { 
+//     background: url('../images/marble_background.jpg') no-repeat center center fixed; 
+//     -webkit-background-size: cover;
+//     -moz-background-size: cover;
+//     -o-background-size: cover;
+//     background-size: cover;
+//   }
+
+// .game-board {
+//     display: flex;
+//     position: relative;
+// }
+
+// .frame-and-canvas {
+//     display: flex;
+//     position: absolute;
+// }
+
+// .puzzle-frame {
+//     display: flex;
+//     position: absolute;
+//     height: 85vh;
+//     width: 65vw;
+//     left: 36px;
+//     top: 30px;
+// }
+
+// .note-buttons {
+//     display: flex;
+//     position: absolute;
+//     flex-direction: column;
+//     right: 118px;
+//     top: 22px;
+// }
+
+// #myCanvas {
+//     display: flex;
+//     position: absolute;
+//     height: 93vh;
+//     width: 64vw;
+//     left: 36px;
+//     top: 30px;
+// }
+
+// .music {
+//     display: flex;
+//     flex-direction: row;
+//     justify-content: center;
+//     position: relative;
+//     left: -7px;
+// }
+
+// #easy {
+//     background-color: green;
+// }
+
+// #medium {
+//     background-color: yellow;
+// }
+
+// #hard {
+//     background-color: red;
+// }
